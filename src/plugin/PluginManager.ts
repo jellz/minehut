e902eb3a5100c8d65fe51316c0c4328a57a41df4
Daniel <danielgulic@gmail.com>
2021-04-14 22:13:54 +1000

    Plugin cache expiry

--- src/plugin/PluginManager.ts
+++ src/plugin/PluginManager.ts
@@ -3,14 +3,16 @@ import { Plugin } from './Plugin';
 import fetch from 'node-fetch';
 import { PluginResponse } from './PluginResponse';
 
+const CACHE_EXPIRY = 1.8e6; // 30 minutes
+
 export class PluginManager {
 	private store: Map<string, Plugin> = new Map();
+	private lastStoreUpdate?: Date;
 
 	constructor(public readonly client: Minehut) {}
 
 	async fetch(idOrName: string): Promise<Plugin | null> {
-		if (this.store.size < 1) await this.fetchAll(true);
-		const found = this.storeToArray().find(
+		const found = (await this.fetchAll()).find(
 			plugin =>
 				idOrName === plugin.id ||
 				idOrName.toLowerCase() === plugin.name.toLowerCase()
@@ -32,12 +34,20 @@ export class PluginManager {
 	}
 
 	async fetchAll(force: boolean = false) {
-		if (!force && this.store.size > 0) return this.storeToArray();
+		const cacheExpired =
+			this.lastStoreUpdate &&
+			Date.now() - this.lastStoreUpdate.getTime() > CACHE_EXPIRY;
+
+		if (!force && this.store.size > 0 && !cacheExpired)
+			return this.storeToArray();
+
 		const res = await fetch(`${this.client.API_BASE}/plugins_public`);
 		if (!res.ok) throw new Error(res.statusText);
 		const pluginsRes: PluginResponse[] = (await res.json()).all;
 		const plugins = pluginsRes.map(i => new Plugin(i));
 		plugins.forEach(plugin => this.store.set(plugin.id, plugin));
+
+		this.lastStoreUpdate = new Date();
 		return this.storeToArray();
 	}
 
